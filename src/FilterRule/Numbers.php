f36bd1faa4ac68e153dc8db7312c785b57a85f5b
Stefan Koopmanschap <stefan@ingewikkeld.net>
2015-06-09 10:11:53 +0200

    tabs? nah

--- src/FilterRule/Numbers.php
+++ src/FilterRule/Numbers.php
@@ -25,6 +25,6 @@ class Numbers extends FilterRule
      */
     public function filter($value)
     {
-		return preg_replace('/\D/', '', $value);
+        return preg_replace('/\D/', '', $value);
     }
 }
