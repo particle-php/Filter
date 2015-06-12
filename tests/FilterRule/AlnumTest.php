bffcf47be4ed99878889c6ad537da11b5f7f2244
Rick van der Staaij <rick@enrise.com>
2015-06-12 16:01:08 +0200

    Add alnum and letters tests

---
+++ tests/FilterRule/AlnumTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Particle\Tests\Filter\FilterRule;
+
+use Particle\Filter\Filter;
+
+class AlnumTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * Prepare the filter
+     */
+    public function setUp()
+    {
+        $this->filter = new Filter();
+    }
+
+    /**
+     * @dataProvider getAlnumResults
+     * @param string $value
+     * @param string $filteredValue
+     */
+    public function testAlnumFilterRule($value, $filteredValue)
+    {
+        $this->filter->value('test')->alnum();
+
+        $result = $this->filter->filter([
+            'test' => $value
+        ]);
+
+        $this->assertEquals($result['test'], $filteredValue);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAlnumResults()
+    {
+        return [
+            ['', ''],
+            ['0123456789', '0123456789'],
+            ['onlylettersinhere', 'onlylettersinhere'],
+            ['0l!e#t$t.e-r+s9', '0letters9'],
+        ];
+    }
+}
